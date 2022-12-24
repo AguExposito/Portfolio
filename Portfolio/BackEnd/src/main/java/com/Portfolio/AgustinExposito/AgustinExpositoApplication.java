091123afc562b0069e318c032406ef5810e88bde
AguExposito <expositoagu@gmail.com>
2022-12-23 22:07:16 -0300

    Added Spring to BackEnd

---
+++ Portfolio/BackEnd/src/main/java/com/Portfolio/AgustinExposito/AgustinExpositoApplication.java
@@ -0,0 +1,13 @@
+package com.Portfolio.AgustinExposito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgustinExpositoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgustinExpositoApplication.class, args);
+	}
+
+}
---
+++ Portfolio/BackEnd/src/test/java/com/Portfolio/AgustinExposito/AgustinExpositoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.AgustinExposito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgustinExpositoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
